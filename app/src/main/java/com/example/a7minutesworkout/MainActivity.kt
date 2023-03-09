4314dd19a3c575af9f37770405c8d3adb4defdec
Darshan Satra <darshansatra1@gmail.com>
2023-03-08 20:03:33 -0500

    add template UI screen with ripple button

---
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener{
+            Toast.makeText(this,"Here",Toast.LENGTH_LONG).show()
+        }
+    }
+}
