a3dc5ca636cbb5fbf7395738cae402a512380c60
Darshan Satra <darshansatra1@gmail.com>
2023-03-19 19:59:48 -0400

    implement room database for history record

--- app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
@@ -2,7 +2,11 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -20,10 +24,29 @@ class FinishActivity : AppCompatActivity() {
 
         binding?.toolbarFinishActivity?.setOnClickListener{
             onBackPressedDispatcher.onBackPressed()
+
         }
 
+        val historyDao = (application as WorkoutApp).db.historyDao()
+
         binding?.btnFinish?.setOnClickListener{
+            addDateToDatabase(historyDao)
             finish()
+        }
+
+
+
+
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao){
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+
+        lifecycleScope.launch{
+            historyDao.insert(HistoryEntity(date))
         }
     }
 
--- app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -3,7 +3,11 @@ package com.example.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
     private var binding:ActivityHistoryBinding? = null
@@ -23,10 +27,37 @@ class HistoryActivity : AppCompatActivity() {
             onBackPressedDispatcher.onBackPressed()
         }
 
+        val dao = (application as WorkoutApp).db.historyDao()
+        getAllDates(dao)
+
     }
 
     override fun onDestroy() {
         binding = null
         super.onDestroy()
+    }
+
+    private fun getAllDates(historyDao: HistoryDao){
+        lifecycleScope.launch {
+            historyDao.fetchAllHistory().collect{
+                if(it.isNotEmpty()){
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+                    val dates = ArrayList<String>()
+                    for(date in it){
+                        dates.add(date.date)
+                    }
+                    val historyAdapter = HistoryAdapter(dates)
+                    binding?.rvHistory?.adapter = historyAdapter
+                }else{
+                    binding?.tvHistory?.visibility = View.INVISIBLE
+                    binding?.rvHistory?.visibility = View.INVISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
